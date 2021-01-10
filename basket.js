48de5de7f6762b9aec7b03e5a76dd569609770a4
SaffronSarb12345678 <75558263+SaffronSarb12345678@users.noreply.github.com>
2021-01-10 11:14:03 +0530

    Update basket.js

--- basket.js
+++ basket.js
@@ -7,7 +7,7 @@ class Baskit{
           }
         
           this.body=Bodies.rectangle(x,y,width,height,options);
-        // World.add(world,this.body);
+         World.add(world,this.body);
           this.width=width;
           this.height=height;
         
